f0e895fa23aaf63a54328c20c9e1491530630e61
Andrew Sprinz <andrew@phenotypic.co.uk>
2012-07-09 22:32:51 +0100

    cache dirs

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
   
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -16,7 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumbnail do
-    process :resize_to_fit => [227,170]
+    process :resize_to_fill => [227,170]
   end
 
   def extension_white_list
